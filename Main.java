1ae60b23f701881de98ed67132bac11f9bc443d0
Ramiz3564 <ramiz3564@wrdsb.ca>
2020-09-15 14:00:03 +0000

    finished practice question 1

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+ import java.util.Scanner;
+
 /**
- *
- * @author 
+ *This program lets the user know if they plugged in an even or odd number
+ * @author Zach Ramirez
  */
 public class Main {
 
@@ -9,7 +11,23 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input 
+    Scanner input = new Scanner(System.in);
     
+    //prompt user for their number
+    System.out.println("Enter your number"); 
+    //intialize user's number
+    int number = input.nextInt();
+    //intialize the remainder
+    int remainder = number % 2; 
+    
+    //Determin whether user's number is odd or even
+    if (remainder >= 1){
+      System.out.println(number + " is an odd number ");
+    }else{
+      System.out.println(number +" is an even number");
+    }   
+
+
   }
 }
